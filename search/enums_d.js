73cd3badaea1a270c0672a3f58a92c6f0e819617
mfrankovi <mfrankovi@users.noreply.github.com>
2025-10-21 14:03:28 +0000

    deploy: 660d5002ab88c307b1e3a06f115c7b41b56611f3

--- search/enums_d.js
+++ search/enums_d.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['negativeunlmodify_0',['NegativeUNLModify',['../classripple_1_1NegativeUNLVote.html#a4fc6405fdb02e58c350e175b6d58e7cf',1,'ripple::NegativeUNLVote']]],
-  ['nodeobjecttype_1',['NodeObjectType',['../namespaceripple.html#a57d7e7b27df7e719fdf5569f96be60a3',1,'ripple']]],
-  ['nodetype_2',['NodeType',['../classripple_1_1Pathfinder.html#a404497923954f689fbde4d0a46fd8304',1,'ripple::Pathfinder']]]
+  ['networktype_1',['NetworkType',['../classripple_1_1NetworkValidators.html#a3bc80c7351c196be801f4400aca1dcc8',1,'ripple::NetworkValidators']]],
+  ['nodeobjecttype_2',['NodeObjectType',['../namespaceripple.html#a57d7e7b27df7e719fdf5569f96be60a3',1,'ripple']]],
+  ['nodetype_3',['NodeType',['../classripple_1_1Pathfinder.html#a404497923954f689fbde4d0a46fd8304',1,'ripple::Pathfinder']]]
 ];
