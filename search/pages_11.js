4ac9aadeaca2e4a7001d11d37ff7f8339ef21c2f
mfrankovi <mfrankovi@users.noreply.github.com>
2025-11-03 15:18:46 +0000

    deploy: 5d5ffb686691c269651c4be0c8926f6f97630d37

--- search/pages_11.js
+++ search/pages_11.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['validator_20exclusion_20list_20system_20documentation_0',['Validator Exclusion List System Documentation',['../md____w_postfiatd_postfiatd_docs_Exclusions.html',1,'']]]
+  ['validator_20exclusion_20list_20system_20documentation_0',['Validator Exclusion List System Documentation',['../md____w_postfiatd_postfiatd_docs_Exclusions.html',1,'']]],
+  ['validator_20key_20and_20domain_20update_20guide_1',['Validator Key and Domain Update Guide',['../md____w_postfiatd_postfiatd_docs_ValidatorKeyUpdate.html',1,'']]]
 ];
